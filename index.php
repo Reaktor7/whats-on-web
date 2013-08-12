4aab87481ba357888053326775eb3dceff3d031e
Mark Charman <mark@reaktor7.com.au>
2013-08-12 21:48:35 +0930

    added maps

--- index.php
+++ index.php
@@ -200,6 +200,42 @@ $app_name = idx($app_info, 'name', '');
                 };
                 var map = new google.maps.Map(document.getElementById("map-canvas"),
                     mapOptions);
+                <?php foreach($events2 as $event):?>
+                var content<?php echo $event['eid'];?> = '<div id="event-<?php echo $event['eid'];?>">'+
+                    '<div id="siteNotice">'+
+                    '</div>'+
+                    '<h2 id="firstHeading" class="firstHeading"><?php echo he($event['name']);?></h2>'+
+                    '<div id="bodyContent">'+
+                    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                    'sandstone rock formation in the southern part of the '+
+                    'Northern Territory, central Australia. It lies 335 km (208 mi) '+
+                    'south west of the nearest large town, Alice Springs; 450 km '+
+                    '(280 mi) by road. Kata Tjuta and Uluru are the two major '+
+                    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                    'Aboriginal people of the area. It has many springs, waterholes, '+
+                    'rock caves and ancient paintings. Uluru is listed as a World '+
+                    'Heritage Site.</p>'+
+                    '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                    'http://en.wikipedia.org/w/index.php?title=Uluru</a> (last visited June 22, 2009).</p>'+
+                    '</div>'+
+                    '</div>';
+
+
+                var infowindow<?php echo $event['eid'];?> = new google.maps.InfoWindow({
+                    content: content<?php echo $event['eid'];?>
+                });
+
+                var marker<?php echo $event['eid'];?> = new google.maps.Marker({
+                    position: new google.maps.LatLng(<?php echo $event['venue']['latitude'];?>,<?php echo $event['venue']['longitude'];?>),
+                    map: map,
+                    title:"<?php echo he($event['name']);?>"
+                });
+
+                google.maps.event.addListener(marker<?php echo $event['eid'];?>, 'click', function() {
+                    infowindow<?php echo $event['eid'];?>.open(map,marker<?php echo $event['eid'];?>);
+                });
+                <?php endforeach;?>
             }
             google.maps.event.addDomListener(window, 'load', initialize);
         </script>
@@ -241,42 +277,6 @@ $app_name = idx($app_info, 'name', '');
         }(document, 'script', 'facebook-jssdk'));
     </script>
     <script type="text/javascript">
-    <?php foreach($events2 as $event):?>
-    var content<?php echo $event['eid'];?> = '<div id="event-<?php echo $event['eid'];?>">'+
-        '<div id="siteNotice">'+
-            '</div>'+
-        '<h2 id="firstHeading" class="firstHeading"><?php echo he($event['name']);?></h2>'+
-        '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                'sandstone rock formation in the southern part of the '+
-                'Northern Territory, central Australia. It lies 335 km (208 mi) '+
-                'south west of the nearest large town, Alice Springs; 450 km '+
-                '(280 mi) by road. Kata Tjuta and Uluru are the two major '+
-                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-                'Aboriginal people of the area. It has many springs, waterholes, '+
-                'rock caves and ancient paintings. Uluru is listed as a World '+
-                'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-                    'http://en.wikipedia.org/w/index.php?title=Uluru</a> (last visited June 22, 2009).</p>'+
-            '</div>'+
-        '</div>';
-
-
-    var infowindow<?php echo $event['eid'];?> = new google.maps.InfoWindow({
-    content: content<?php echo $event['eid'];?>
-    });
-
-    var marker<?php echo $event['eid'];?> = new google.maps.Marker({
-    position: new google.maps.LatLng(<?php echo $event['venue']['latitude'];?>,<?php echo $event['venue']['longitude'];?>),
-    map: map,
-    title:"<?php echo he($event['name']);?>"
-    });
-
-    google.maps.event.addListener(marker<?php echo $event['eid'];?>, 'click', function() {
-    infowindow<?php echo $event['eid'];?>.open(map,marker<?php echo $event['eid'];?>);
-    });
-    <?php endforeach;?>
     </script>
     <header class="clearfix">
         <?php if (isset($basic)) { ?>
