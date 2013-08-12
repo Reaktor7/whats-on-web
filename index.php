c9b5c289695e64afa9bc18ef458e4484ec5e01d4
Mark Charman <mark@reaktor7.com.au>
2013-08-12 22:08:06 +0930

    added maps

--- index.php
+++ index.php
@@ -206,9 +206,10 @@ $app_name = idx($app_info, 'name', '');
                     '</div>'+
                     '<h2 id="firstHeading" class="firstHeading"><?php echo he($event['name']);?></h2>'+
                     '<div id="bodyContent">'+
+                    '<img style="float:left;" src="<?php echo he($event['pic_big']);?>"/>'+
                     '<p><b>Location:</b> <?php echo he($event['location']);?></p>'+
-                    '<p><b>Start Time: </b><?php echo he($event['location']);?></p>'+
-                    '<p><b>End Time: </b><?php echo he($event['location']);?></p>'+
+                    '<p><b>Start Time: </b><?php echo he($event['start_time']);?></p>'+
+                    '<p><b>End Time: </b><?php echo he($event['end_time']);?></p>'+
                     '<p><?php echo str_replace(array("\r\n", "\n", "\r"), '<br />', he($event['description']));?></p>'+
                     '</div>'+
                     '</div>';
